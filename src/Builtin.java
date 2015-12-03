53888e7cb9116c13014e7c8b4b3ddd23d7b6eda6
Ozukami <arthur.poisson@ymail.com>
2015-12-03 20:14:05 +0100

    Début de cd
    Fonctionnel : "cd .."
    Correction de tokenize_command() (y'avait un soucis sur le split)
    Dans execute_command, dans les if : j'ai remplacé les argl.equals(...)
    par argv.get(0).equals(...) pour tester uniquement le 1er mot de la
    commande
    PS: J'suis sous Windows là, donc hésite pas à vérifier si ça marche bien
    chez toi aussi :)

--- src/Builtin.java
+++ src/Builtin.java
@@ -1,45 +1,47 @@
-import java.util.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.File;
+import java.util.ArrayList;
 
 /* Classe Builtin
- *
- * Contient les commandes internes du shell (ls, ps, pwd, cd)
- */
+*
+* Contient les commandes internes du shell (ls, ps, pwd, cd)
+*/
 public class Builtin{
-
-	public static void execute_commande_exit(ArrayList<String> argv){
-		System.exit(0);
-	}
-
-	public static void execute_commande_ls(ArrayList<String> argv){
-		File dossier = new File(System.getProperty("user.dir"));
-		String enfants[] = dossier.list();
-		
-		for(String fils: enfants){
-			System.out.println(fils);
-		}	
-	} 
-
-	public static void execute_commande_ps(ArrayList<String> argv){
-		System.out.println("Execution de ps");
-	} 
-
-	public static void execute_commande_pwd(ArrayList<String> argv){
-		String pwd = System.getProperty("user.dir");
-		System.out.println(pwd);
-	} 
-
-	public static void execute_commande_cd(ArrayList<String> argv){
-		System.out.println("Execution de cd");
-	} 
-
-	public static void execute_commande_date(ArrayList<String> argv){
-		System.out.println("Execution de date");
-	} 
-
-	public static void execute_commande_find(ArrayList<String> argv){
-		System.out.println("Execution de find");
-	} 
-
+    
+    public static void execute_commande_exit(ArrayList<String> argv){
+        System.exit(0);
+    }
+    
+    public static void execute_commande_ls(ArrayList<String> argv){
+        File dossier = new File(System.getProperty("user.dir"));
+        String enfants[] = dossier.list();
+        
+        for(String fils: enfants){
+            System.out.println(fils);
+        }
+    }
+    
+    public static void execute_commande_ps(ArrayList<String> argv){
+        System.out.println("Execution de ps");
+    }
+    
+    public static void execute_commande_pwd(ArrayList<String> argv){
+        String pwd = System.getProperty("user.dir");
+        System.out.println(pwd);
+    }
+    
+    public static void execute_commande_cd(ArrayList<String> argv){
+        System.out.println("Execution de cd");        
+        File dossier = new File(System.getProperty("user.dir"));
+        if(argv.get(1).equals(".."))
+            System.setProperty("user.dir", dossier.getParentFile().toString());
+    }
+    
+    public static void execute_commande_date(ArrayList<String> argv){
+        System.out.println("Execution de date");
+    }
+    
+    public static void execute_commande_find(ArrayList<String> argv){
+        System.out.println("Execution de find");
+    }
+    
 }
--- src/Shell_Project.java
+++ src/Shell_Project.java
@@ -1,84 +1,92 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class Shell_Project{
-
-	private Scanner sc;
-	private String argl; // Ligne de commande brute
-	private ArrayList<String> argv; // Liste des arguments
-
-	/*
-	 * Constructeur
-	 */
-	public Shell_Project(){
-		sc = new Scanner(System.in);
-		argv = new ArrayList<String>(); 
-
-		// Boucle infinie du shell (S'arrête grâce à Ctrl+D ou bien exit)
-		while(true){
-			display_prompt();
-			read_command();
-			tokenize_command();
-			execute_command();
-		}
-	}
-
-	/*
-	 * Affiche une nouvelle ligne pour la prochaine commande
-	 */
-	public void display_prompt(){
-		System.out.print("MyShell$ ");
-	}
-
-	/*
-	 * Stock la commande complete dans argl
-	 */
-	public void read_command(){
-		try{
-			argl = sc.nextLine();
-
-		}catch (NoSuchElementException e){ // Exception levé lors de Ctrl+D
-			argl = "exit";
-			System.out.println("exit");
-		}
-	}
-
-	/*
-	 * Découpe la commande en arguments distincts
-	 */
-	public void tokenize_command(){
-		argv = new ArrayList<String>(Arrays.asList(argl.split("\\s*,\\s*")));
-	}
-
-	/*
-	 * Execute la commande contenue dans argl
-	 */
-	public void execute_command(){
-		if(argl.equals("exit"))
-			Builtin.execute_commande_exit(argv);
-		else if(argl.equals("ls")){
-			Builtin.execute_commande_ls(argv);
-		}
-		else if(argl.equals("ps")){
-			Builtin.execute_commande_ps(argv);
-		}
-		else if(argl.equals("pwd")){
-			Builtin.execute_commande_pwd(argv);
-		}
-		else if(argl.equals("cd")){
-			Builtin.execute_commande_cd(argv);
-		}
-		else if(argl.equals("date")){
-			Builtin.execute_commande_date(argv);
-		}
-		else if(argl.equals("find")){
-			Builtin.execute_commande_find(argv);
-		}
-		else{
-			System.out.format("La commande %s n'existe pas.\n", argl);
-		}
-	}
-
-	public static void main(String[] args){
-		Shell_Project shell = new Shell_Project();
-	}
+    
+    private Scanner sc;
+    private String argl; // Ligne de commande brute
+    private ArrayList<String> argv; // Liste des arguments
+    
+    /*
+    * Constructeur
+    */
+    public Shell_Project(){
+        sc = new Scanner(System.in);
+        argv = new ArrayList<String>();
+        
+        // Boucle infinie du shell (S'arrête grâce à Ctrl+D ou bien exit)
+        while(true){
+            display_prompt();
+            read_command();
+            tokenize_command();
+            execute_command();
+        }
+    }
+    
+    /*
+    * Affiche une nouvelle ligne pour la prochaine commande
+    */
+    public void display_prompt(){
+        System.out.print("MyShell$ ");
+    }
+    
+    /*
+    * Stock la commande complete dans argl
+    */
+    public void read_command(){
+        try{
+            argl = sc.nextLine();
+            
+        }catch (NoSuchElementException e){ // Exception levé lors de Ctrl+D
+            argl = "exit";
+            System.out.println("exit");
+        }
+    }
+    
+    /*
+    * Découpe la commande en arguments distincts
+    */
+    public void tokenize_command(){
+        argv = new ArrayList<String>(Arrays.asList(argl.split("\\s+")));
+    }
+    
+    /*
+    * Execute la commande contenue dans argl
+    */
+    public void execute_command(){
+        if(argv.get(0).equals("exit"))
+            Builtin.execute_commande_exit(argv);
+        else if(argv.get(0).equals("ls")){
+            Builtin.execute_commande_ls(argv);
+        }
+        else if(argv.get(0).equals("ps")){
+            Builtin.execute_commande_ps(argv);
+        }
+        else if(argv.get(0).equals("pwd")){
+            Builtin.execute_commande_pwd(argv);
+        }
+        else if(argv.get(0).equals("cd")){
+            if(argv.size()<2)
+                System.out.println("Pas assez d'arguments -> cd <path>");
+            else if(argv.size()>2)
+                System.out.println("Trop d'arguments -> cd <path>");
+            else
+                Builtin.execute_commande_cd(argv);
+        }
+        else if(argv.get(0).equals("date")){
+            Builtin.execute_commande_date(argv);
+        }
+        else if(argv.get(0).equals("find")){
+            Builtin.execute_commande_find(argv);
+        }
+        else{
+            System.out.format("La commande %s n'existe pas.\n", argl);
+        }
+    }
+    
+    public static void main(String[] args){
+        Shell_Project shell = new Shell_Project();
+    }
 }
